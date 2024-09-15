5f81c0b414a54a526e9cc1961434ee6eef322022
psychic-coder <rohitganguly450@gmail.com>
2024-09-15 09:44:26 +0530

    made the header section

--- moviesapp/src/app/page.js
+++ moviesapp/src/app/page.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>page</div>
+    <div>
+      HomePage
+    </div>
   )
 }
 
---
+++ moviesapp/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import MenuItem from './MenuItem'
+import {AiFillHome} from "react-icons/ai"
+import {BsFillInfoCircleFill} from "react-icons/bs"
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+        <div className="flex gap-4">
+            <MenuItem title="Home" address="/" Icon={AiFillHome} />
+            <MenuItem title="About" address="/about" Icon={BsFillInfoCircleFill} />
+        </div>
+        <Link href={"/"} className="flex  items-center bg-amber-500 rounded-lg">
+            <span className="text-2xl font-bold  py-1 px-2 rounded-lg">MoDic</span>
+        </Link>
+    </div>
+  )
+}
+
+export default Header
