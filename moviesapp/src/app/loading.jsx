924d72b862186fd75adc3fafa0ff124ad1c76890
psychic-coder <rohitganguly450@gmail.com>
2024-09-22 13:33:24 +0530

    added the loading effect

---
+++ moviesapp/src/app/loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Yes, in Next.js (specifically in the App Router architecture), the loading.jsx file is automatically used to show a loading UI when data is being fetched
+const loading = () => {
+  return (
+    <div className="flex justify-center mt-16">
+        <img className="h-52 " src="Spinner.svg" alt="loading" />
+    </div>
+  )
+}
+
+export default loading
--- moviesapp/src/app/page.js
+++ moviesapp/src/app/page.js
@@ -4,12 +4,14 @@ const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || 'fetchTrending';
+  
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=en-US&page=1`,
     { next: { revalidate: 10000 } }
   );
+  // the revalidate method written above is tused to refetch the data after every 10000 seconds
   const data = await res.json();
   if (!res.ok) {
     throw new Error('Failed to fetch data');
