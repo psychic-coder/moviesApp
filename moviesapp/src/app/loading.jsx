1ae4acf6af87af3d7288f90c10402001cfb53fc5
psychic-coder <rohitganguly450@gmail.com>
2024-09-22 13:44:25 +0530

    made the loading effect

--- moviesapp/src/app/loading.jsx
+++ moviesapp/src/app/loading.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 // Yes, in Next.js (specifically in the App Router architecture), the loading.jsx file is automatically used to show a loading UI when data is being fetched
 const loading = () => {
   return (
-    <div className="flex justify-center mt-16">
-        <img className="h-52 " src="Spinner.svg" alt="loading" />
-    </div>
+    <div className='flex justify-center mt-16'>
+    <img className='h-52' src='spinner.svg' alt='loading...' />
+  </div>
   )
 }
 
