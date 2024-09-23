6ab16a8fe59b3773c5dd793586e0a88eef9060c9
psychic-coder <rohitganguly450@gmail.com>
2024-09-23 17:24:48 +0530

    correct usesearchPARAMS

--- moviesapp/src/components/NavbarItem.jsx
+++ moviesapp/src/components/NavbarItem.jsx
@@ -1,24 +1,28 @@
-"use client";
+"use client"; // Ensures this is a client-side component
+import Loading from "@/app/loading";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 
 const NavbarItem = ({ title, param }) => {
   const searchParams = useSearchParams();
   const genre = searchParams.get("genre");
+
   return (
-    <div>
-      <Link
-        className={`hover:text-amber-600 font-semibold ${
-          genre === param
-            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
-            : ""
-        }`}
-        href={`/?genre=${param}`}
-      >
-        {title}
-      </Link>
-    </div>
+    <Suspense fallback={<Loading/>}>
+      <div>
+        <Link
+          className={`hover:text-amber-600 font-semibold ${
+            genre === param
+              ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+              : ""
+          }`}
+          href={`/?genre=${param}`}
+        >
+          {title}
+        </Link>
+      </div>
+    </Suspense>
   );
 };
 
