927f0cae5e8a372c34da3c1c6a437309c86a4945
psychic-coder <rohitganguly450@gmail.com>
2024-09-23 17:32:13 +0530

    modified the home page

--- moviesapp/src/app/page.js
+++ moviesapp/src/app/page.js
@@ -1,26 +1,41 @@
+"use client"
 import Results from '@/components/Results';
+import { Suspense } from 'react';
+import Loading from './loading';
 
 const API_KEY = process.env.API_KEY;
 
+
 export default async function Home({ searchParams }) {
-  const genre = searchParams.genre || 'fetchTrending';
-  
+  const genre = searchParams?.genre || 'fetchTrending';
+
+
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=en-US&page=1`,
     { next: { revalidate: 10000 } }
   );
-  // the revalidate method written above is tused to refetch the data after every 10000 seconds
-  const data = await res.json();
+
+  
   if (!res.ok) {
     throw new Error('Failed to fetch data');
   }
+
+  const data = await res.json();
   const results = data.results;
 
+  
   return (
     <div>
-      <Results results={results} />
+      <Suspense fallback={<div><Loading/></div>}>
+        <ResultsWrapper results={results} />
+      </Suspense>
     </div>
   );
+}
+
+
+function ResultsWrapper({ results }) {
+  return <Results results={results} />;
 }
