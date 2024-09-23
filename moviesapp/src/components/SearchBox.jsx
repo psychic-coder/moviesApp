bfa9fdcf89e3db7a9557448d3eff0205b6eda403
psychic-coder <rohitganguly450@gmail.com>
2024-09-23 17:01:05 +0530

    made the search box

---
+++ moviesapp/src/components/SearchBox.jsx
@@ -0,0 +1,32 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const SearchBox = () => {
+  const router=useRouter();
+  const [search, setSearch] = useState("");
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    router.push(`/search/${search}`);
+
+  }
+  return (
+    <form className="flex justify-between px-5 max-w-6xl mx-auto" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search keywords..."
+        className="w-full h-14 rounded-md placeholder-gray-500 outline-none bg-transparent flex-1 "
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button
+        className="text-amber-600 disabled:text-gray-400"
+        disabled={search === ""}
+      >
+        Search
+      </button>
+    </form>
+  );
+};
+
+export default SearchBox;
