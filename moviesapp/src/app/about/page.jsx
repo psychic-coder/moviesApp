6ff39df472e20bf5e10ca579a29b8b6a31d9f299
psychic-coder <rohitganguly450@gmail.com>
2024-09-23 17:19:34 +0530

    solved the error in the about page

--- moviesapp/src/app/about/page.jsx
+++ moviesapp/src/app/about/page.jsx
@@ -3,37 +3,33 @@ export default function About() {
     <div className="max-w-6xl mx-auto p-3 space-y-4">
       <h1 className="text-2xl font-medium text-amber-600">About</h1>
       <p>
-        {" "}
         Welcome to MoDic, your premier destination for all things related to
         movies. As a team of dedicated movie enthusiasts, we have come together
         to build a comprehensive platform where you can explore and discover a
-        world of cinema.{" "}
-      </p>{" "}
+        world of cinema.
+      </p>
       <p>
-        {" "}
         At MoDic, we are committed to offering an extensive and up-to-date
         database of films from across the globe. Our platform provides detailed
         information on the latest releases, news, reviews, and trailers. Whether
-        you're searching by title, director, actor, genre, or release date,
+        you&apos;re searching by title, director, actor, genre, or release date,
         MoDic is designed to help you easily find the perfect film for any
-        occasion.{" "}
-      </p>{" "}
+        occasion.
+      </p>
       <p>
-        {" "}
         Beyond our expansive movie catalog, MoDic is also a hub for film
         lovers to connect and engage. Our community forum offers a space where
         users can discuss their favorite films, exchange opinions, and explore
         reviews and ratings from other members. Additionally, our dedicated
-        sections for movie news and trailers ensure you're always informed about
-        the latest trends and developments in the cinematic world.{" "}
-      </p>{" "}
+        sections for movie news and trailers ensure you&apos;re always informed
+        about the latest trends and developments in the cinematic world.
+      </p>
       <p>
-        {" "}
         Thank you for choosing MoDic. We are constantly striving to enhance
         your experience on our platform, and we welcome your feedback and
         suggestions. Should you have any questions or ideas, please do not
         hesitate to reach out. We hope you enjoy exploring our movie database
-        and connecting with fellow movie aficionados. Happy browsing!{" "}
+        and connecting with fellow movie aficionados. Happy browsing!
       </p>
     </div>
   );
